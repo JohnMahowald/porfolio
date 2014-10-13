8ea801cff31b521656794ad8c30d1c0fdd551dea
CrookedKings <John.Mahowald@gmail.com>
2014-10-12 17:02:56 -0700

    sidebar is sticky, links scroll with jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,57 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.scrollTo.min
+// require jquery.sticky
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+  var $window = $(window)
+  
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+  
+  $('[data-target=#projects-sub]').click(function(e) {
+    $('#projects-sub').toggle('collapse');
+  });
+  
+  /* Window Resizing */
+  var setDimensions = function() {
+    $('.main-section').css('height', $window.height());
+    $('.title-wrapper').css('padding-top', $window.height() / 3);
+    $('.main-nav-wrapper').css('padding-top', $window.height() / 3);
+    $('#sidebar').css('height', $window.height());
+  }
+  
+  setDimensions();
+  
+  window.onresize = function() { setDimensions() };
+  
+  /* Fixed Menu Settings */
+  var sidebarTop = $('#sidebar').offset().top
+  
+  var stickySidebar = function() {
+    var scrollTop = $(window).scrollTop();
+    
+    if (scrollTop > sidebarTop) {
+      $('#sidebar').addClass('fixed');
+    } else {
+      $('#sidebar').removeClass('fixed');
+    }
+  };
+  
+  stickySidebar();
+  
+  $(window).scroll(function() {
+    stickySidebar()
+  })
+  
+  /* Scrolling */
+  $('a').click( function(e) {
+    var target = $(e.currentTarget).data('link-target')
+    if (target) { $(window).scrollTo(target, 600) };
+  })
+});
