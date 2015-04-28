59b035ef6610b78ef8d6af10ccc07f7cde32e12b
John Mahowald <john.mahowald@gmail.com>
2015-04-28 01:07:22 -0700

    remove seting dimensions with javascript. yuck

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,6 +1,7 @@
 $(document).ready( function() {
   $('a').click( function(e) {
     var target = $(e.currentTarget).data('link-target')
+
     if (target) { 
       e.preventDefault();
       $(window).scrollTo(target, 800);
